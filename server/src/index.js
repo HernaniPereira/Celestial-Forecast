23d2243c78824c126881ab78fb4676ff93f14781
HernaniPereira <pereirahernani9@gmail.com>
2021-04-14 00:22:36 +0100

    creating server

---
+++ server/src/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+const mongoUri =
+  "mongodb+srv://admin:passwordadmin@cluster0.tsw85.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.on("connected", () => {
+  console.log("Connected to mongo instance");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error("Error connecting to mongo", err);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hi there!");
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
